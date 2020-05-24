9bd916b56020e668b2b71f9e19dafd9108db35ad
andersonpatrignani <anderson.patrignani@gmail.com>
2020-05-24 20:36:10 -0300

    no message

---
+++ src/YourFinances.Authentication.Infra.DataBase/Tables/Account.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Identification] NCHAR(250) NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 0
+)
---
+++ src/YourFinances.Authentication.Infra.DataBase/Tables/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [Identification] VARCHAR(150) NOT NULL, 
+    [Active] BIT NOT NULL DEFAULT 0, 
+    [AcceptTerm] BIT NOT NULL DEFAULT 0, 
+    [Email] VARCHAR(200) NOT NULL, 
+    [AccountId] INT NOT NULL, 
+    [Password] VARCHAR(MAX) NOT NULL, 
+    [Salt] VARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_User_Account] FOREIGN KEY ([AccountId]) REFERENCES Account(Id) 
+)
